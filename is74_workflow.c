498929eba0d7fb0bed7648f9927aeea25a7a7c21
Askhat Baimukhamedov <34022611+askhatbaimukhamedov@users.noreply.github.com>
2021-11-10 01:57:16 +0500

    Update is74_workflow.c

--- is74_workflow.c
+++ is74_workflow.c
@@ -1,6 +1,6 @@
 /***********************************************************************************************************************************************
- *  is74_workflow.c - is a main workpoint for IS74 programming team
- *  2019 Intersviyaz (*)> quack quack ...
+ *  is74_workflow.c - is a main workpoint for IS74 programming team                                                                            *
+ *  2019 Intersviyaz (*)> quack quack ...                                                                                                      *
  ***********************************************************************************************************************************************/
 
 #include "is74_workflow.h"
@@ -26,45 +26,38 @@ uint8_t tmp[268]                 = {0};                                         
 
 
 /* Main cycle function for IS74 team, runs cyclically in the main loop */
-void is74_mainloop(void)
-{
+void is74_mainloop(void) {
    /* Response on request. Command handler */
    if(dm_to_is74_main_var.LoRaRxCompleteFlag)
      request_handler(dm_to_is74_main_var.LoRaRxBuff, dm_to_is74_main_var.LoRaRxBuffSize); 
   
-   switch (dm_to_is74_main_var.State)
-   { 
+   switch (dm_to_is74_main_var.State) { 
      case IS74_STATE_IDLE: break;
      
      /* The main case of the program */
-     case IS74_STATE_WAIT_FOR_RESPONSE:
-     {
+     case IS74_STATE_WAIT_FOR_RESPONSE: {
         /* Strategy work relay */
        if(dm_to_is74_main_var.UnixTime != 0x00)
          onoff_relay(dm_to_is74_main_var.UnixTime);
        
         /* Scanning of target device */
        if(dm_to_is74_main_var.LoRaTxTimerFlag || Common.flag_relay_status 
-                                || Common.request_04 || Common.request_05)
-       {       
+                                              || Common.request_04 || Common.request_05) {       
          /* Save the first request time */
          if(dm_to_is74_main_var.LoRaTxTimerFlag)
            Common.t = (!Common.t) ? dm_to_is74_main_var.UnixTime : Common.t;
          
          /* Send data to the LoRa server */
-         if (dm_to_is74_main_var.LoRaTxComplete && Common.is_send)
-         {
+         if (dm_to_is74_main_var.LoRaTxComplete && Common.is_send) {
            Common.index_com++;           
-           switch(Common.whoami)
-           {     
+           switch(Common.whoami) {     
              //case energy_mera: send_lora_energy_mera(); break;
              case mercury: send_lora_mercury(); break;
            }
          } 
          
          /* Scanning of target device */
-         else if (dm_to_is74_main_var.RxComplete && !Common.is_send)
-         {
+         else if (dm_to_is74_main_var.RxComplete && !Common.is_send) {
            dm_to_is74_main_var.RxComplete = false;
            Common.complete = 1;
            
@@ -72,11 +65,9 @@ void is74_mainloop(void)
            is74_delay(DEL_COM_ENR, DEL_COM_MER);                                             
            Common.index_com++;
          }       
-         if (dm_to_is74_main_var.TimerAlarm && Common.complete)
-         {
+         if (dm_to_is74_main_var.TimerAlarm && Common.complete) {
            dm_to_is74_main_var.TimerAlarm = false;       
-           switch(Common.whoami)
-           {     
+           switch(Common.whoami) {     
              //case energy_mera: run_energy_mera(); break;
              case mercury: run_mercury(); break;          
              default: is74__OneTime();
@@ -87,10 +78,8 @@ void is74_mainloop(void)
      
      /* Upon successful connection of the module to the 
         network variable state = IS74_STATE_RS485_CONNECT */
-     case IS74_STATE_WAIT_FOR_JOIN:
-     {
-       if (dm_to_is74_main_var.LoRaJoinState == LORA_JOINST_JOINED)
-       {
+     case IS74_STATE_WAIT_FOR_JOIN: {
+       if (dm_to_is74_main_var.LoRaJoinState == LORA_JOINST_JOINED) {
          /* Setting the start time */
          uint32_t time = 0;
          dm_API_StartUnixTimer(time);
@@ -110,10 +99,8 @@ void is74_mainloop(void)
      
      /* Connect to the target device and transfer 
         control to case: IS74_STATE_WAIT_FOR_RESPONSE */
-     case IS74_STATE_RS485_CONNECT:
-     {
-       switch(Common.whoami)
-       {
+     case IS74_STATE_RS485_CONNECT: {
+       switch(Common.whoami) {
          //case energy_mera: connect_energy_mera(); break;
          case mercury: connect_mercury(); break;
        }
@@ -121,8 +108,7 @@ void is74_mainloop(void)
      } break; 
      
      /* If you here check LoRaWAN NET  */
-     case IS74_STATE_NO_LORAWAN_NET:
-     {
+     case IS74_STATE_NO_LORAWAN_NET: {
        is74__OneTime();
        break;                                                                                
      }
